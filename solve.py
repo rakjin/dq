e68b01c11c75e565364509c165da7ffdaf7fe215
rakjin <grampush@gmail.com>
2014-12-23 20:31:38 +0900

    refactoring

--- solve.py
+++ solve.py
@@ -1,92 +1,90 @@
 
 class Node:
-
 	def __init__(self, altitude):
 		self.altitude = altitude
 		self.adjacents = []
 		self.is_destination = False
 
-	def add_adjacent_node(self, node):
+	def add_adjacent(self, node):
 		self.adjacents.append(node)
 
-	# def __iter__(self):
-	# 	for node in self.adjacents:
-	# 		if node.visit_possible(from_=self):
-	# 			clone = Node(self.altitude)
-	# 			clone.adjacents = adjacents
-	# 			yield node
-	# 	raise StopIteration()
-
 	def remove_unvisitable_adjacents(self):
-		visitable_adjacents = \
-			[node for node in self.adjacents if node.is_visitable(from_=self)]
-		self.adjacents = visitable_adjacents
-
+		visitables = \
+			filter(lambda x: x.is_visitable(from_=self), self.adjacents)
+		self.adjacents = visitables
 
 	def is_visitable(self, from_):
-		if self in from_.adjacents and \
-		   from_.altitude >= self.altitude:
+		if (self in from_.adjacents) and \
+		   (from_.altitude >= self.altitude):
 		   	return True
 		return False
 
 	def __repr__(self):
 		return '<Node altitude=%d>' % self.altitude
 
-input = '2 5\n\
-88 99 44 33 22\n\
-77 66 55 33 11'
 
+def parse_and_get_first_node(raw):
+	rows = raw.splitlines()
 
-rows = input.splitlines()
+	dimension = rows.pop(0)
+	dimension = dimension.split(' ')
+	height = int(dimension[0])
+	width = int(dimension[1])
 
-dimension = rows.pop(0)
-dimension = dimension.split(' ')
-height = int(dimension[0])
-width = int(dimension[1])
+	for y in range(0, len(rows)):
+		row = rows[y] = rows[y].split(' ')
+		for x in range(0, len(row)):
+			altitude = row[x] = int(row[x])
+			node = row[x] = Node(altitude)
 
+			if x > 0:
+				left = row[x-1]
+				node.add_adjacent(left)
+				left.add_adjacent(node)
 
+			if y > 0:
+				upper = rows[y-1][x]
+				node.add_adjacent(upper)
+				upper.add_adjacent(node)
 
-for y in range(0, len(rows)):
-	row = rows[y] = rows[y].split(' ')
-	for x in range(0, len(row)):
-		altitude = row[x] = int(row[x])
-		node = row[x] = Node(altitude)
+	flat = [item for sublist in rows for item in sublist] # googled how to flatten
 
-		if x > 0:
-			left = row[x-1]
-			node.add_adjacent_node(left)
-			left.add_adjacent_node(node)
+	first_node = flat[0]
+	last_node = flat[-1]
+	last_node.is_destination = True
 
-		if y > 0:
-			upper = rows[y-1][x]
-			node.add_adjacent_node(upper)
-			upper.add_adjacent_node(node)
+	[node.remove_unvisitable_adjacents() for node in flat]
 
-flat = [item for sublist in rows for item in sublist] # googled
+	return first_node
 
-last_node = flat[-1]
-last_node.is_destination = True
-
-[node.remove_unvisitable_adjacents() for node in flat]
-
-node0 = flat[0]
-
-
+# visit recursively and return possible paths' count within its trials
 def visit(node, path):
 	if node in path:
-		return
-	print('visiting %s' % node)
+		return 0
 	path.append(node)
 	if node.is_destination:
-		print('destination')
-		#print(path)
+		# found possible path
 		path.remove(node)
-		return
+		return 1
+	count = 0
 	for i in range(0, len(node.adjacents)):
 		adjacent = node.adjacents[i]
-		visit(adjacent, path)
-	#print('dead end')
+		count += visit(adjacent, path)
+	# dead end
 	path.remove(node)
+	return count
 
-path = []
-visit(node0, path)
+
+if __name__ == '__main__':
+
+	raw = '5 3\n\
+	22 22 22 11 11\n\
+	22 22 22 11 11\n\
+	11 11 22 22 22'
+
+	first_node = parse_and_get_first_node(raw)
+
+	path = []
+	count = visit(first_node, path)
+
+	print (count)
